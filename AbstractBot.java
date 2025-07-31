e0fd32e926a618edafd8ece95a3a965ba5855ed9
jakob-rzeppa <-g>
2025-07-31 22:50:40 +0200

    switched to SortedMap for the current Bets

--- AbstractBot.java
+++ AbstractBot.java
@@ -1,14 +1,15 @@
+import java.util.SortedMap;
+
 import actions.Action;
 
 abstract class AbstractBot {
     public abstract String getName();
 
-    public abstract Action play(
-        int ownPosition, 
-        int[] playerMoney,
-        int moneyInPot, 
-        int[] currentBets, 
-        Card[] ownCards, 
-        Card[] uncoveredCards
+    public abstract Action selectAction(
+        int bankroll,
+        Card[] ownCards,
+        Card[] uncoveredCards,
+        int moneyInPot,
+        SortedMap<Integer, Integer> currentBets
     );
 }
